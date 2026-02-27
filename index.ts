7a90ff9f50290da7a15cc0269c0bb542946aeda9
Vaibhav Bisht <56401767+meanOtaku@users.noreply.github.com>
2026-02-27 11:41:56 +0530

    Update protocol
    TCP to WS

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
-import { createServer } from 'node:net'
 import { Aedes } from 'aedes'
 import express, { type Request, type Response } from "express";
 
-const port = process.env.PORT;
+import { createServer } from 'aedes-server-factory'
 
-const aedes = await Aedes.createBroker()
-const server = createServer(aedes.handle);
+const port = process.env.PORT;
 const port_broker = process.env.PORT_BROKER;
 const app = express();
+const aedes = await Aedes.createBroker()
+const httpServer = createServer(aedes, { ws: true })
 
-server.listen(port_broker, () => {
-    console.log(`✅ MQTT Broker running on port ${port_broker}`);
-});
+httpServer.listen(port_broker, function () {
+    console.log('✅ MQTT websocket server listening on port:', port_broker)
+})
 
 
 app.get("/", (req: Request, res: Response) => {
@@ -26,6 +26,11 @@ app.listen(port, () => {
 aedes.on('client', (client) => {
     console.log('Client Connected: ', client.id);
 });
+
+aedes.off('client', (client) => {
+    console.log("Client Connected: ", client.id);
+
+})
 
 aedes.on('publish', (packet, client) => {
     if (client) {
